9736da347e5a44997243a7dbdefd35ad3cabec9b
Michelangelo Pezzini <lds.pezzini@gmail.com>
2026-09-05 22:00:16 -0300

    Updates week 01

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-import express from 'express';
+import { fileURLToPath } from "url";
+import path from "path";
+import express from "express";
 
 // Define the application environment
-const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "production";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,37 +14,39 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 /**
-  * Configure Express middleware
-  */
+ * Configure Express middleware
+ */
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 /**
  * Routes
  */
-app.get('/', async (req, res) => {
-    const title = 'Home';
-    res.render('home', { title });
+app.get("/", async (req, res) => {
+  const title = "Home";
+  res.render("home", { title });
 });
 
-app.get('/organizations', async (req, res) => {
-    const title = 'Our Partner Organizations';
-    res.render('organizations', { title });
+app.get("/organizations", async (req, res) => {
+  const title = "Our Partner Organizations";
+  res.render("organizations", { title });
 });
 
-app.get('/projects', async (req, res) => {
-    const title = 'Service Projects';
-    res.render('projects', { title });
+app.get("/projects", async (req, res) => {
+  const title = "Service Projects";
+  res.render("projects", { title });
 });
-
+app.get("/categories", async (req, res) => {
+  const title = "Service Project Categories";
+  res.render("categories", { title });
+});
 
 // Set EJS as the templating engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Tell Express where to find your templates
-app.set('views', path.join(__dirname, 'src/views'));
+app.set("views", path.join(__dirname, "src/views"));
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://127.0.0.1:${PORT}`);
